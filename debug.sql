f2f79af0a489964f3670c4530fd3f4a60a41cc41
Filip Lubniewski <flubniewski@hotmail.com>
2024-04-02 23:20:36 +0200

    update debug.sql with better data

--- debug.sql
+++ debug.sql
@@ -2,14 +2,19 @@ use local;
 select * from streams;
 select * from events;
 
+SET @uuid = '6ccd780c-baba-1026-9564-5b8c656024db';
+
 INSERT INTO streams (id, type, version)
-SELECT 100, 'test', 0
-WHERE NOT EXISTS (SELECT 1 FROM streams WHERE id = 100 AND version = 0);
+SELECT UUID_TO_BIN(@uuid), 'test', 0
+WHERE NOT EXISTS (SELECT 1 FROM streams WHERE id = UUID_TO_BIN(@uuid) AND version = 0);
 
-INSERT INTO events (id, stream_id, version, data, type)
-SELECT 200, 100, 1, '{}', 'test'
-WHERE NOT EXISTS (SELECT 1 FROM streams WHERE id = 100 AND version = 0);
+START TRANSACTION;
+SET @uuid = '6ccd780c-baba-1026-9564-5b8c656024db';
+INSERT INTO events (stream_id, version, data, type)
+SELECT UUID_TO_BIN(@uuid), 1, '{"key": "value"}', 'test'
+WHERE EXISTS (SELECT 1 FROM streams WHERE id = UUID_TO_BIN(@uuid) AND version = 0);
+COMMIT;
+SELECT * FROM events;
 
-INSERT INTO events (id, stream_id, version, data, type)
-SELECT UUID_TO_BIN(UUID()), 100, 1, '{"key": "value"}', 'test'
-WHERE NOT EXISTS (SELECT 1 FROM streams WHERE id = 100 AND version = 0);
+
+SELECT * FROM streams WHERE id = UUID_TO_BIN(@uuid) AND version = 0;

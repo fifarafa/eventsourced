148bc09f716ab1e5aa3b1fe2dedf93fe1f5d9d0a
Filip Lubniewski <flubniewski@hotmail.com>
2024-04-13 23:24:25 +0200

    add another test case

--- main_test.go
+++ main_test.go
@@ -82,3 +82,36 @@ func TestAppendingConsecutiveEvents(t *testing.T) {
 		t.Errorf("failed to append event: %v", err)
 	}
 }
+
+func TestAppendingEventWithOldVersion(t *testing.T) {
+	// given
+	if err := setup(); err != nil {
+		t.Errorf("setup failed: %v", err)
+	}
+
+	db, err := openMySQLConnection()
+	if err != nil {
+		t.Errorf("failed to open database: %v", err)
+	}
+
+	rawMsg := json.RawMessage(`{"key": "value"}`)
+
+	// when & then
+	streamID := uuid.New()
+	if err := appendSingleEvent(db, streamID, "invoice", rawMsg, -1); err != nil {
+		t.Errorf("failed to append event: %v", err)
+	}
+
+	if err := appendSingleEvent(db, streamID, "invoice", rawMsg, 0); err != nil {
+		t.Errorf("failed to append event: %v", err)
+	}
+
+	if err := appendSingleEvent(db, streamID, "invoice", rawMsg, 1); err != nil {
+		t.Errorf("failed to append event: %v", err)
+	}
+
+	err = appendSingleEvent(db, streamID, "invoice", rawMsg, 0)
+	if err == nil {
+		t.Errorf("expected error, got nil")
+	}
+}

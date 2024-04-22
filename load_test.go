f091506a518951bb7740f65a7e040aa4948eab04
Filip Lubniewski <flubniewski@hotmail.com>
2024-04-22 23:31:02 +0200

    start test but with many failures

--- load_test.go
+++ load_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
+	"sync"
 	"testing"
 )
 
@@ -108,5 +109,35 @@ func TestLoadStreamWithBigAmountOfEvents(t *testing.T) {
 }
 
 func TestLoadStreamInConcurrentEnvironment(t *testing.T) {
+	// given
+	if err := setup(); err != nil {
+		t.Errorf("setup failed: %v", err)
+	}
 
+	db, err := openMySQLConnection()
+	if err != nil {
+		t.Errorf("failed to open database: %v", err)
+	}
+
+	rawMsg := json.RawMessage(`{"key": "value"}`)
+	streamID := uuid.New()
+
+	wg := sync.WaitGroup{}
+	wg.Add(1000)
+	for i := 0; i < 1000; i++ {
+		go func() {
+			wg.Add(1)
+			defer wg.Done()
+			if err := appendSingleEvent(db, streamID, "invoice", rawMsg, int64(i-1)); err != nil {
+				t.Errorf("failed to append event: %v", err)
+			}
+		}()
+	}
+	wg.Wait()
+	// when & then
+	events, err := loadStream(db, streamID)
+	if err != nil {
+		t.Errorf("failed to load stream: %v", err)
+	}
+	assert.Len(t, events, 1000)
 }
